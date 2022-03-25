49c024843f67e8362c9eedcd4ebc7fb68cd6f6ce
eugenejac3 <eugene.jackson9@gmail.com>
2022-03-24 21:06:14 -0500

    deploy2

--- config/config.js
+++ config/config.js
@@ -1,24 +1,10 @@
 require('dotenv').config();
 module.exports = {
-  "development": {
+  "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
-  "test": {
-    "username": "ohjyjzop",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "production": {
-    "username": "ohjyjzop",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
 }
